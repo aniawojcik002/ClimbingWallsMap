b53ddfd40dc95573b7f42b525efeefc1ab25b9aa
ania.wojcik002 <anna.wojcik002@gmail.com>
2023-02-21 15:58:44 +0100

    Add basic routing ✨feature: Add basic routing and new login component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
+
 import MapComponent from "./MapComponent";
 import Header from "./Header";
+import Login from './Login';
 
 function App() {
   return (
     <>
       <Header />
-      <MapComponent/>
+      <Routes>
+        <Route path="/" element={<MapComponent />} />
+          <Route path="/map" element={<MapComponent />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={"Page not found"} />
+      </Routes>
     </>
   );
 }
---
+++ src/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Here will be Login Page</div>
+  )
+}
+
+export default Login
