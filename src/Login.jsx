34f653af65d68179f0109c8ba95154627c3a5ca1
ania.wojcik002 <anna.wojcik002@gmail.com>
2023-02-23 23:14:30 +0100

    feat: Add styling to login page and global font family
    Added styles to Login Page and add font family to whole App and Popups

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,30 +5,33 @@ import styles from "./Login.module.css";
 const Login = () => {
   return (
     <div className={styles.mainWrapper}>
-      <div>
-        <p>Welcome back! Please enter your details.</p>
-        <div className={styles.loginWrapper}>
-          <div className={styles.labelWrapper}>
-            <label for="username">Username</label>
-            <label for="pass">Password</label>
-          </div>
+      <div className={styles.wrapper}>
+        <div className={styles.textWrapper}>
+          <h3 className={styles.welcomeText}>
+            Welcome back!</h3>
+          <h4 className={styles.secondaryText}>Welcome back! Please enter your details.</h4>
+        </div>
           <div className={styles.inputWrapper}>
             <input
+              placeholder="Username"
               type="text"
               id="username"
               name="username"
+              autocomplete="username"
               className={styles.username}
             ></input>
             <input
+              placeholder="Password"
               type="password"
               id="pass"
+              autocomplete="current-password"
               name="password"
               className={styles.password}
             ></input>
           </div>
-        </div>
+        {/* </div> */}
         <div>
-          <button type="submit">Submit</button>
+          <button className={styles.loginButton} type="submit" disabled="disabled">Log in</button>
         </div>
       </div>
     </div>
